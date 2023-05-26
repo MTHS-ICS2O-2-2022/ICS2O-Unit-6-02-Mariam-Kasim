488e85dc397f774cadab45cc6ccd40ac2be4e8a3
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-26 17:27:48 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked () {
+  //This function determines how many times the cookie has been clicked
+  
+  //process
+  if (localStorage.clicks) {
+    localStorage.clicks = Number(localStorage.clicks) + 1
+  }
+  else {
+    localStorage.clicks = 1
+  }
+
+  //output
+  document.getElementById("answer").innerHTML = " You have "+localStorage.clicks+" cookies! "
+}
